b25ebfd839b77b3ecbee10210a73f281eac9c8fa
zavanton123 <zavanton123@gmail.com>
2021-06-17 18:03:45 +0300

    deployment to google cloud

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# build the prod images
+docker build -t zavanton/famount-web:latest -t zavanton/famount-web:$SHA -f ./client/Dockerfile ./client
+docker build -t zavanton/famount-api:latest -t zavanton/famount-api:$SHA -f ./server/Dockerfile ./server
+
+# push the prod images to Docker Hub (with the latest tag)
+docker push zavanton/famount-web:latest
+docker push zavanton/famount-api:latest
+
+# push the prod images to Docker Hub (with the git sha tag)
+docker push zavanton/famount-web:$SHA
+docker push zavanton/famount-api:$SHA
+
+# apply the k8s configs located in the ./k8s directory
+kubectl apply -f k8s
+
+# update the images in the k8s cluster
+kubectl set image deployments/server-deployment server=zavanton/famount-api:$SHA
+kubectl set image deployments/client-deployment client=zavanton/famount-web:$SHA
+### (end deploy.sh)
